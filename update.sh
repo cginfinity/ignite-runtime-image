5d43a9d780a2e8760142601f38f8f835c779b5a1
Bijay Shah <bijay.shah@cybergindia.local>
2020-06-26 18:26:29 +0530

    update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+heroku login
+heroku apps
+read -p 'Enter App Name: ' appname
+heroku container:login
+heroku container:push web --app $appname
+heroku container:release web --app $appname
+read -p 'Press Enter To Exit: ' $appname
