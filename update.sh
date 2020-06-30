c772ba63aae88028925749c57907692fc6e5024f
bijay shah <bijayshah@outlook.com>
2020-06-30 11:29:54 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ read -p 'Enter App Name: ' appname
 heroku container:login
 heroku container:push web --app $appname
 heroku container:release web --app $appname
-read -p 'Press Enter To Exit: ' $appname
+read -p 'Press Enter To Exit: ' q
